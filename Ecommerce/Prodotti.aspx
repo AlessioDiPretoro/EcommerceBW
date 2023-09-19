cb86c439e3d4ab524691367f0134b7fb8e6788c4
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-09-19 16:49:19 +0200

    galleria prodotti

--- Ecommerce/Prodotti.aspx
+++ Ecommerce/Prodotti.aspx
@@ -3,5 +3,47 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Pagina prodotti</h1>
+    <h1 class="text-center my-3">Pagina prodotti</h1>
+    <div class="d-flex justify-content-center align-items-center w-100">
+<%--        <a href="categoria.aspx?=idCategoria=" clss="btn btn-primary">content</a>--%>
+<%--        <a href="categoria.aspx?=idCategoria=" clss="btn btn-primary">content</a>--%>
+<%--        <a href="categoria.aspx?=idCategoria=" clss="btn btn-primary">content</a>--%>
+<%--        <a href="categoria.aspx?=idCategoria=" clss="btn btn-primary">content</a>--%>
+<%--        <a href="categoria.aspx?=idCategoria=" clss="btn btn-primary">content</a>--%>
+    </div>
+    <div class="container">
+        <div class="row row-cols-4">
+            <asp:Repeater runat="server" ID="Repeater1" ItemType="Ecommerce.Product">
+                <ItemTemplate>
+                    <div class="col my-3">
+                        <div class="card">
+                            <img src="<%# Item.Copertina %>" 
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <%-- Nome prodotto --%>
+                                    <%# Item.NomeProdotto %>
+                                </h5>
+                                <hr />
+                                <p class="card-text">
+                                    <%-- peso --%>
+                                    <%# Item.Peso %>
+                                </p>
+                                <hr />
+                                <p class="card-text">
+                                    <%-- descrizione breve --%>
+                                    <%# Item.DescrizioneBreve %>
+                                </p>
+                                <hr />
+                                <p class="card-text">
+                                    <%-- prezzo --%>
+                                    <%# Item.PrezzoBase %>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </div>
 </asp:Content>
--- Ecommerce/Prodotti.aspx.cs
+++ Ecommerce/Prodotti.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,64 @@ namespace Ecommerce
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string Connection = ConfigurationManager.ConnectionStrings["DB_ConnString"].ConnectionString.ToString();
+            SqlConnection sql= new SqlConnection(Connection);
 
+            SqlCommand cmd = new SqlCommand("select * from prodotti", sql);
+            SqlDataReader sqlDataReader;
+
+            try
+            {
+                
+                sql.Open();
+
+                List<Product> ListaProdotti=new List<Product>();
+                sqlDataReader=cmd.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Product Product = new Product();
+                    Product.Copertina = sqlDataReader["copertina"].ToString();
+                    Product.IdProdotto = Convert.ToInt32(sqlDataReader["idprodotto"]);
+                    Product.NomeProdotto = sqlDataReader["nomeProdotto"].ToString();
+                    Product.Peso = Convert.ToDouble(sqlDataReader["peso"]);
+                    Product.DescrizioneBreve = sqlDataReader["descrizioneBreve"].ToString();
+                    Product.PrezzoBase = Convert.ToDouble(sqlDataReader["prezzoBase"]);
+                    ListaProdotti.Add(Product);
+                }
+                Repeater1.DataSource = ListaProdotti; 
+                Repeater1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+
+            }finally {  sql.Close(); }
         }
     }
+
+
+    public class Product
+    {
+        public int IdProdotto { get; set; }
+        public double PrezzoBase { get; set; }
+        public double Peso { get; set; }
+        public string NomeProdotto { get; set; }
+        public string DescrizioneBreve { get; set; }
+
+        public string TipoplogiaAnimale { get; set; }
+        public string PaeseOrigine { get; set; }
+        public string Copertina { get; set; }
+        public string Immagine1 { get; set; }
+        public string Immagine2 { get; set; }
+        public string Immagine3 { get; set; }
+        public string Immagine4 { get; set; }
+        public int PzzDisponibili { get; set; }
+        public DateTime DataInserimento { get; set; }
+
+        public bool InEvidenza { get; set; }
+        public int ScontoPercentuale { get; set; }
+    }
+
+    
 }
